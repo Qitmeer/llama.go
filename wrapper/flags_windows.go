1f0a8c176f3786cdbc886fd3ff79ea05ed0f3602
r74tech <r74tech@gmail.com>
2025-11-11 18:01:08 +0900

    chore: update LDFLAGS in flags_windows.go for improved linking
    - Modified LDFLAGS to include -Wl,--start-group and -Wl,--end-group for better handling of circular dependencies in linked libraries.
    - Added mingwex and mingw32 to LDFLAGS to enhance compatibility with Windows builds.

--- wrapper/flags_windows.go
+++ wrapper/flags_windows.go
@@ -8,6 +8,6 @@ package wrapper
 #cgo CXXFLAGS: -std=c++17 -D_UCRT -D__USE_MINGW_ANSI_STDIO=0
 #cgo CFLAGS: -I${SRCDIR}/../core/include
 #cgo CXXFLAGS: -I${SRCDIR}/../core/include
-#cgo LDFLAGS: -L${SRCDIR}/../build/lib -lllama_core -lcommon -lllama -lwhisper -lwhisper-common -lmtmd -l:ggml.a -l:ggml-base.a -l:ggml-cpu.a -lstdc++ -lws2_32 -lwinpthread -lpthread -lucrt
+#cgo LDFLAGS: -L${SRCDIR}/../build/lib -Wl,--start-group -lllama_core -lcommon -lllama -lwhisper -lwhisper-common -lmtmd -l:ggml.a -l:ggml-base.a -l:ggml-cpu.a -lstdc++ -lws2_32 -lwinpthread -lpthread -lucrt -lmingwex -lmingw32 -Wl,--end-group
 */
 import "C"
